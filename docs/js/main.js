24a8365a162fc1cd64b3627742c029ec8360d529
Evgeniia Garnaga <evgarnaga@gmail.com>
2020-04-08 07:11:16 +0600

    добавляет сборку к docs

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,6 +3,7 @@
 (function () {
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
+  var logoMenu = document.querySelector('.logo-menu');
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -11,9 +12,13 @@
     )) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
+      logoMenu.classList.remove('logo-menu--closed');
+      logoMenu.classList.add('logo-menu--opened');
     } else {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
+      logoMenu.classList.add('logo-menu--closed');
+      logoMenu.classList.remove('logo-menu--opened');
     }
   });
 })(document.body);

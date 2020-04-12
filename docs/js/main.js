cdf6f53d28deff5b2143aec4b87ff3ca9dc5c1cf
Evgeniia Garnaga <evgarnaga@gmail.com>
2020-04-12 19:24:53 +0600

    Вносит правки (Ранее бронирование, label)

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,7 +2,7 @@
 
 (function () {
 
-  // Hamburger Menu
+  // Меню для мобильной версии
 
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
@@ -25,11 +25,11 @@
     }
   });
 
-  // Маска
+  // Маска для номера телефона
 
-  window.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('DOMContentLoaded', function () {
     var inputs = document.querySelectorAll('input[type="tel"]');
-    Array.prototype.forEach.call(inputs, function(input) {
+    Array.prototype.forEach.call(inputs, function (input) {
       new InputMask({
         selector: input, // в качестве селектора может быть элемент, или, собственно css селектор('#input', '.input', 'input'). Если селектор - тег или класс - будет получен только первый элемент
         layout: input.dataset.mask
@@ -39,75 +39,74 @@
   })
 
   function InputMask(options) {
-      this.el = this.getElement(options.selector);
-      if (!this.el) return console.log('Что-то не так с селектором');
-      this.layout = options.layout || '+_ (___) ___-__-__';
-      this.maskreg = this.getRegexp();
+    this.el = this.getElement(options.selector);
+    if (!this.el) return console.log('Что-то не так с селектором');
+    this.layout = options.layout || '+_ (___) ___-__-__';
+    this.maskreg = this.getRegexp();
 
-      this.setListeners();
+    this.setListeners();
   }
 
-  InputMask.prototype.getRegexp = function() {
-      var str = this.layout.replace(/_/g, '\\d')
-      str = str.replace(/\(/g, '\\(')
-      str = str.replace(/\)/g, '\\)')
-      str = str.replace(/\+/g, '\\+')
-      str = str.replace(/\s/g, '\\s')
+  InputMask.prototype.getRegexp = function () {
+    var str = this.layout.replace(/_/g, '\\d');
+    str = str.replace(/\(/g, '\\(');
+    str = str.replace(/\)/g, '\\)');
+    str = str.replace(/\+/g, '\\+');
+    str = str.replace(/\s/g, '\\s');
 
-      return str;
+    return str;
+  };
+
+  InputMask.prototype.mask = function (e) {
+    var _this = e.target,
+        matrix = this.layout,
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = _this.value.replace(/\D/g, "");
+
+    if (def.length >= val.length) val = def;
+
+    _this.value = matrix.replace(/./g, function(a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a
+    });
+
+    if (e.type == 'blur') {
+      var regexp = new RegExp(this.maskreg);
+      if (!regexp.test(_this.value)) _this.value = '';
+    } else {
+      this.setCursorPosition(_this.value.length, _this);
+    }
+  };
+
+  InputMask.prototype.setCursorPosition = function (pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+      var range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+    }
+  };
+
+  InputMask.prototype.setListeners = function () {
+    this.el.addEventListener("input", this.mask.bind(this), false);
+    this.el.addEventListener("focus", this.mask.bind(this), false);
+    this.el.addEventListener("blur", this.mask.bind(this), false);
   }
 
-  InputMask.prototype.mask = function(e) {
-      var _this = e.target,
-          matrix = this.layout,
-          i = 0,
-          def = matrix.replace(/\D/g, ""),
-          val = _this.value.replace(/\D/g, "");
+  InputMask.prototype.getElement = function (selector) {
+    if (selector === undefined) return false;
+    if (this.isElement(selector)) return selector;
+    if (typeof selector === 'string') {
+      var el = document.querySelector(selector);
+      if (this.isElement(el)) return el;
+    }
+    return false;
+  };
 
-      if (def.length >= val.length) val = def;
-
-      _this.value = matrix.replace(/./g, function(a) {
-          return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
-      });
-
-      if (e.type == "blur") {
-          var regexp = new RegExp(this.maskreg);
-          if (!regexp.test(_this.value)) _this.value = "";
-      } else {
-          this.setCursorPosition(_this.value.length, _this);
-      }
-  }
-
-  InputMask.prototype.setCursorPosition = function(pos, elem) {
-      elem.focus();
-      if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-      else if (elem.createTextRange) {
-          var range = elem.createTextRange();
-          range.collapse(true);
-          range.moveEnd("character", pos);
-          range.moveStart("character", pos);
-          range.select()
-      }
-  }
-
-  InputMask.prototype.setListeners = function() {
-      this.el.addEventListener("input", this.mask.bind(this), false);
-      this.el.addEventListener("focus", this.mask.bind(this), false);
-      this.el.addEventListener("blur", this.mask.bind(this), false);
-  }
-
-  InputMask.prototype.getElement = function(selector) {
-      if (selector === undefined) return false;
-      if (this.isElement(selector)) return selector;
-      if (typeof selector == 'string') {
-          var el = document.querySelector(selector);
-          if (this.isElement(el)) return el;
-      }
-      return false
-  }
-
-  InputMask.prototype.isElement = function(element) {
-      return element instanceof Element || element instanceof HTMLDocument;
-  }
-
-})(document.body);
+  InputMask.prototype.isElement = function (element) {
+    return element instanceof Element || element instanceof HTMLDocument;
+  };
+})();
